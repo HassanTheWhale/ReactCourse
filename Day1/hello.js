86d9be56664a39f30df54c46718e0c078bdfeeab
Hassan Sh. AlShammari <33877200+HassanTheWhale@users.noreply.github.com>
2021-10-08 17:59:23 +0300

    First Part

---
+++ Day1/hello.js
@@ -0,0 +1,46 @@
+//log on console
+console.log("=-=-=-=-=-=-[ First Part ]-=-=-=-=-=-=");
+
+//create a function with a parameter
+function greeter(name) {
+    console.log("Hello " + name + ", ");
+}
+
+//call the function with a parameter
+greeter("Hassan")
+
+//another function
+function sum(x, y) {
+    let result = x + y;
+    console.log("The sum is: " + result);
+}
+
+//another call
+sum(3,1)
+
+//Create a variable
+let ta = "Waleed :D";
+
+// print the variable
+console.log(ta);
+
+//other variables and printing
+let x = 7;
+let y = 8;
+console.log(x+y)
+
+//decalre an object
+let amazingPerson = {
+    name: "Ali Ramadan",
+    age: 22,
+    major: "CE",
+    level: -1 //-1 is MAX
+}
+
+// print info from the last object
+console.log(
+    "Name: " + amazingPerson.name
+    + "\nAge: " + amazingPerson.age
+    + "\nMajor: " + amazingPerson.major
+);
+

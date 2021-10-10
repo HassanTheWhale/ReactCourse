c13548be20156d9c7a82716747277aea5745ad00
Hassan Sh. AlShammari <33877200+HassanTheWhale@users.noreply.github.com>
2021-10-10 19:49:31 +0300

    Big Components

--- Day2/hello-world/src/App.js
+++ Day2/hello-world/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
+      <BigCard title="Hello World" text="Hello World, We are students in the react course that is presented by the amazing Teacher Eng. Manawer Al-Azmi" src="https://wallpaperaccess.com/full/3865610.jpg"/>
       <Card title="First" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime quisquam minima amet, non cum numquam ab quasi itaque inventore architecto ipsam qui laudantium rerum rem soluta in consequuntur sequi, mollitia reprehenderit repellendus! Totam nemo mollitia nihil dignissimos ullam explicabo sint libero quaerat maxime dolorem, itaque quod dolorum error? Voluptatem." />
       <Card title="Second" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime quisquam minima amet, non cum numquam ab quasi itaque inventore architecto ipsam qui laudantium rerum rem soluta in consequuntur sequi, mollitia reprehenderit repellendus! Totam nemo mollitia nihil dignissimos ullam explicabo sint libero quaerat maxime dolorem, itaque quod dolorum error? Voluptatem."/>
       <Card title="Third" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime quisquam minima amet, non cum numquam ab quasi itaque inventore architecto ipsam qui laudantium rerum rem soluta in consequuntur sequi, mollitia reprehenderit repellendus! Totam nemo mollitia nihil dignissimos ullam explicabo sint libero quaerat maxime dolorem, itaque quod dolorum error? Voluptatem."/>
@@ -11,7 +12,7 @@ function App() {
 }
 
 function Card(props) {
-  console.log(props)
+  console.log(props);
   return <div className="Card">
       <h1>{props.title}</h1>
       {/* <p>{props.text}</p> */}
@@ -24,10 +25,20 @@ function Card(props) {
 }
 
 function CardParagraph(props) {
-  console.log(props)
+  console.log(props);
   return (
     <p>{props.text}</p>
   );
 }
 
+function BigCard(props) {
+  console.log(props); 
+  return <div className="BigCard">
+    <img src={props.src} alt={props.title} />
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.text}</p>
+    </div>
+  </div>
+}
 export default App;

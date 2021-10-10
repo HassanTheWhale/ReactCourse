60f6558377de63aa8d2e6531095b6fbc8c3b9b83
Hassan Sh. AlShammari <33877200+HassanTheWhale@users.noreply.github.com>
2021-10-10 19:20:51 +0300

    Edit our component more and more

--- Day2/hello-world/src/App.js
+++ Day2/hello-world/src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Card />
-    </div>
+    <>
+      <Card title="First" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime quisquam minima amet, non cum numquam ab quasi itaque inventore architecto ipsam qui laudantium rerum rem soluta in consequuntur sequi, mollitia reprehenderit repellendus! Totam nemo mollitia nihil dignissimos ullam explicabo sint libero quaerat maxime dolorem, itaque quod dolorum error? Voluptatem." />
+      <Card title="Second" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime quisquam minima amet, non cum numquam ab quasi itaque inventore architecto ipsam qui laudantium rerum rem soluta in consequuntur sequi, mollitia reprehenderit repellendus! Totam nemo mollitia nihil dignissimos ullam explicabo sint libero quaerat maxime dolorem, itaque quod dolorum error? Voluptatem."/>
+      <Card title="Third" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime quisquam minima amet, non cum numquam ab quasi itaque inventore architecto ipsam qui laudantium rerum rem soluta in consequuntur sequi, mollitia reprehenderit repellendus! Totam nemo mollitia nihil dignissimos ullam explicabo sint libero quaerat maxime dolorem, itaque quod dolorum error? Voluptatem."/>
+    </>
   );
 }
 
-function Card() {
-  return (
-    <div className="Card">
-      <h1>Title</h1>
-      <div className=""></div>
+function Card(props) {
+  console.log(props)
+  return <div className="Card">
+      <h1>{props.title}</h1>
+      <p>{props.text}</p>
+      <div className="">
+        {/* print special chars */}
+        {/* {"{}"} */}
+      </div>
     </div>
-  );
+  
 }
 
 export default App;

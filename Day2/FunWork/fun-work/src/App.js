fba46cea53c75a3f5c290201a5b1a7f2ae63e85e
Hassan Sh. AlShammari <hassanl22@hotmail.com>
2021-10-11 16:45:51 +0300

    Base of App.js

--- Day2/FunWork/fun-work/src/App.js
+++ Day2/FunWork/fun-work/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header src="https://cdn.discordapp.com/attachments/884950893391380483/897116803245940756/72.png" name="TheWhale" />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+function Header(props) {
+  return (
+    <div className="header">
+      <div className="content">
+        <img src={props.src} alt={props.name} />
+        <h1>{props.name}</h1>
+      </div>
+    </div>
+  );
+}
+
+function Body(props) {
+  return (
+      <div className="body">
+        <h2>My Hobbies</h2>
+        <Card title="Programming" src="https://cdn.discordapp.com/attachments/884950893391380483/897117403383742484/undraw_programming_2svr.svg" text="This is the txt" />
+        <Card title="Sleeping" src="https://cdn.discordapp.com/attachments/884950893391380483/897117620430589962/undraw_Loading_re_5axr.svg" text="This is the txt" />
+        <Card title="Eating" src="https://cdn.discordapp.com/attachments/884950893391380483/897117219169894400/undraw_Hamburger_8ge6.svg" text="This is the txt"  />
+      </div>
+    );
+}
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      Copyright 2021. Made with <span>❤</span> by H. TheWhale 🐳.
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.src} alt={props.src}/>
+      <h3>{props.title}</h3>
+      <p>{props.text}</p>
     </div>
   );
 }

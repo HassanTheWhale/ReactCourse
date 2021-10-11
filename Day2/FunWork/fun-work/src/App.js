8678b6e445aa9ba2858e6ef5ef34aeeccd3e58ba
Hassan Sh. AlShammari <hassanl22@hotmail.com>
2021-10-11 16:58:57 +0300

    Edit App.css and js

--- Day2/FunWork/fun-work/src/App.js
+++ Day2/FunWork/fun-work/src/App.js
@@ -14,8 +14,8 @@ function Header(props) {
   return (
     <div className="header">
       <div className="content">
+        <h2>{props.name}</h2>
         <img src={props.src} alt={props.name} />
-        <h1>{props.name}</h1>
       </div>
     </div>
   );
@@ -24,10 +24,12 @@ function Header(props) {
 function Body(props) {
   return (
       <div className="body">
-        <h2>My Hobbies</h2>
-        <Card title="Programming" src="https://cdn.discordapp.com/attachments/884950893391380483/897117403383742484/undraw_programming_2svr.svg" text="This is the txt" />
-        <Card title="Sleeping" src="https://cdn.discordapp.com/attachments/884950893391380483/897117620430589962/undraw_Loading_re_5axr.svg" text="This is the txt" />
-        <Card title="Eating" src="https://cdn.discordapp.com/attachments/884950893391380483/897117219169894400/undraw_Hamburger_8ge6.svg" text="This is the txt"  />
+        <h2 className="title center">My Hobbies</h2>
+        <div className="flexCards">
+          <Card title="Programming" src="https://cdn.discordapp.com/attachments/884950893391380483/897117403383742484/undraw_programming_2svr.svg" text="Programming was a hobby for me. Until.. I found myself a CS Student @ KU and an known freelancer" />
+          <Card title="Watching" src="https://cdn.discordapp.com/attachments/884950893391380483/897120216000110732/undraw_cms_re_asu0.svg" text="This is the txt" />
+          <Card title="Eating" src="https://cdn.discordapp.com/attachments/884950893391380483/897117219169894400/undraw_Hamburger_8ge6.svg" text="This is the txt"  />
+        </div>
       </div>
     );
 }
@@ -44,7 +46,7 @@ function Card(props) {
   return (
     <div className="card">
       <img src={props.src} alt={props.src}/>
-      <h3>{props.title}</h3>
+      <h2 className="center">{props.title}</h2>
       <p>{props.text}</p>
     </div>
   );

dd4548e2a70face914e51fdfd16ae8b57d148995
Hassan Sh. AlShammari <33877200+HassanTheWhale@users.noreply.github.com>
2021-10-12 23:32:14 +0300

    base of funwork 3

--- Day3/fun-work/src/App.js
+++ Day3/fun-work/src/App.js
@@ -1,6 +1,37 @@
 import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
+  let planets = [
+    {
+      name: "Name Of Planet 1",
+      desc: "Desc. Of Planet 1",
+      distance: "100Km",
+      theme: "dark",
+      imgSrc: "link here",
+    },
+    {
+      name: "Name Of Planet 2",
+      desc: "Desc. Of Planet 2",
+      distance: "210Km",
+      theme: "light",
+      imgSrc: "link here",
+    },
+    {
+      name: "Name Of Planet 3",
+      desc: "Desc. Of Planet 3",
+      distance: "1000Km",
+      theme: "dark",
+      imgSrc: "link here",
+    },
+    {
+      name: "Name Of Planet 4",
+      desc: "Desc. Of Planet 4",
+      distance: "2121KM",
+      theme: "",
+      imgSrc: "link here",
+    },
+  ];
   return <></>;
 }
 
